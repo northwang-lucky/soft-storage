cfdf18b3c3c421293413a8f2e8629031788c4c0a
North Wang <wyb_goodluck@163.com>
2023-02-02 13:29:42 +0800

    chore: add basic development tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: 'airbnb-typescript-prettier',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-var-requires': 'off',
+    'func-names': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-extend-native': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['result', 'itemRef'],
+      },
+    ],
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'react/function-component-definition': 'off',
+    'react-hooks/rules-of-hooks': 'off',
+  },
+};
