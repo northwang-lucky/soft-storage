e4d17c5d9283aa742a8807047ba3ee4c87c46685
North Wang <wyb_goodluck@163.com>
2023-02-03 10:54:05 +0800

    refactor(hooks): delete unused code

--- packages/hooks/src/discard-storage/discarded-keys-storage.ts
+++
@@ -1,6 +0,0 @@
-import { createLocalStorage } from '../create-storage';
-
-export const useStorage = createLocalStorage<Partial<Record<string, boolean>>>({
-  rootNodeKey: '__discarded_keys__',
-  initial: {},
-});
