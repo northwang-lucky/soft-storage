751f59ac44bb8d237131e1b0cf1647a43a09267e
North Wang <wyb_goodluck@163.com>
2023-02-04 15:20:18 +0800

    chore(coverage): change coverage script

--- scripts/coverage.cjs
+++ scripts/coverage.cjs
@@ -1,5 +1,11 @@
-const open = require('open');
+const server = require('live-server');
 const { resolve } = require('path');
 
 const packageName = process.env.npm_package_config_name;
-open(`file://${resolve(__dirname, `../packages/${packageName}/coverage/lcov-report/index.html`)}`);
+const options = {
+  port: 5176,
+  root: resolve(__dirname, `../packages/${packageName}/coverage/lcov-report`),
+  file: 'index.html',
+  open: true,
+};
+server.start(options);
