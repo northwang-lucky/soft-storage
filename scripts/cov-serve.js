c04c109ae136fdb131fcd48f496dd710e2f1a770
North Wang <wyb_goodluck@163.com>
2023-02-05 11:29:55 +0800

    ci(coverage-upload): add coverage-upload workflow

--- scripts/cov-serve.js
+++ scripts/cov-serve.js
@@ -1,10 +1,10 @@
 const server = require('live-server');
-const { resolve } = require('path');
+const path = require('path');
 
 const packageName = process.env.npm_package_config_name;
 const options = {
   port: 5176,
-  root: resolve(__dirname, `../packages/${packageName}/coverage/lcov-report`),
+  root: path.resolve(__dirname, `../packages/${packageName}/coverage/lcov-report`),
   file: 'index.html',
   open: true,
 };
